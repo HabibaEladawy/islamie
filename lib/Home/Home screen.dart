73df79c5c35b9fe9858d6c543e4a4772145be37c
habiba <beboaladawy@gmail.com>
2024-12-10 13:11:11 +0200

    initial project. Added empty home screen.

---
+++ lib/Home/Home screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  static const String routeName = 'Home Screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
